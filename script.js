ccdc9d539560f1a72139de0423bd1ba051292298
Jake Walker <jcw2865@gmail.com>
2020-01-10 00:20:16 -0600

    Created script.js. Added password generator to portfolio.

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    var passwordGenerator = $("#passwordGenerator");
+    passwordGenerator.on("click", function (event) {
+        event.preventDefault();
+        window.open("https://jcw2865.github.io/homework-3/");
+    })
+})
