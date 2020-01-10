dd45a29720a70480ee66d94fb005e4f2b058441f
Jake Walker <jcw2865@gmail.com>
2020-01-10 00:37:17 -0600

    Created links and thumbnail images for items added to portfolio.

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 $(document).ready(function () {
+    // Create click event to take user to password generator
     var passwordGenerator = $("#passwordGenerator");
     passwordGenerator.on("click", function (event) {
         event.preventDefault();
         window.open("https://jcw2865.github.io/homework-3/");
+    })
+    // Create click event to take user to day planner
+    var dayPlanner = $("#dayPlanner");
+    dayPlanner.on("click", function (event) {
+        event.preventDefault();
+        window.open("https://jcw2865.github.io/homework-5/");
+    })
+    // Create click event to take user to star wars quiz
+    var starWarsQuiz = $("#starWarsQuiz");
+    starWarsQuiz.on("click", function (event) {
+        event.preventDefault();
+        window.open("https://jcw2865.github.io/homework-4/");
     })
 })
