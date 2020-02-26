d9bdffec130707e2cdaa9cd7f8d9051c8d131e84
Jake Walker <jcw2865@gmail.com>
2020-02-25 19:08:50 -0600

    Created an 'About' page to test router functionality. Works. Needs styling.

--- jake-walker-portfolio/src/App.js
+++ jake-walker-portfolio/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/About";
@@ -12,20 +11,8 @@ function App() {
     <Router>
       <div className="App">
         <Wrapper>
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-        </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-        </a>
-          </header>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
         </Wrapper>
       </div>
     </Router>
--- jake-walker-portfolio/src/pages/About.js
+++ jake-walker-portfolio/src/pages/About.js
@@ -1,62 +1,60 @@
 import React from "react";
-import Hero from "../components/Hero";
-import Container from "../components/Container";
-import Row from "../components/Row";
-import Col from "../components/Col";
 
 function About() {
   return (
     <div>
-      <Hero backgroundImage="https://i.imgur.com/qkdpN.jpg">
-        <h1>Pupster</h1>
-        <h2>They're the Good Boys and Girls</h2>
-      </Hero>
-      <Container style={{ marginTop: 30 }}>
-        <Row>
-          <Col size="md-12">
-            <h1>Welcome To Pupster!</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-12">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
-              consequat mauris ullamcorper eu. Orci varius natoque penatibus et magnis dis
-              parturient montes, nascetur ridiculus mus. Pellentesque et dui id justo finibus
-              sollicitudin at et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
-              gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis mi sollicitudin
-              turpis semper, et interdum risus lobortis. Vestibulum suscipit nunc non egestas
-              tristique. Proin hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
-              quam non, tristique tempor erat. Nullam non sem facilisis, tempus tortor sit amet,
-              volutpat nisl. Ut et turpis non nunc maximus mollis a vitae tortor. Pellentesque
-              mattis risus ac quam laoreet cursus. Praesent suscipit orci neque, vestibulum
-              tincidunt augue tincidunt non. Duis consequat mattis tortor vitae mattis.
-            </p>
-            <p>
-              Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum porttitor. Sed
-              malesuada molestie velit ac viverra. Quisque a ullamcorper purus. Curabitur luctus mi
-              ac mi hendrerit semper. Nulla tincidunt accumsan lobortis. Mauris convallis sapien non
-              nibh porta accumsan. Nunc volutpat tempus porttitor. Nunc congue dictum egestas.
-              Aliquam blandit mi eu urna scelerisque, vitae volutpat ligula ultricies. Maecenas vel
-              porta augue. Fusce mauris ex, dignissim et lacinia ut, tempus eget nibh.
-            </p>
-            <p>
-              Etiam ut massa efficitur, gravida sapien non, condimentum sapien. Suspendisse massa
-              tortor, facilisis in neque sit amet, scelerisque elementum tortor. Nullam eget nibh
-              sit amet odio lobortis ullamcorper. Nulla bibendum magna nec sem pulvinar lobortis.
-              Mauris et imperdiet urna, vitae lobortis dui. Nunc elementum elit mi, non mattis enim
-              congue at. Proin mi lectus, ullamcorper id hendrerit eu, ultricies vitae lacus. Nunc
-              vehicula, erat eget laoreet condimentum, felis ante malesuada leo, nec efficitur diam
-              nisi eget nisi. Cras arcu lacus, tristique in bibendum vitae, elementum eget lorem.
-              Maecenas vestibulum volutpat orci eu pharetra. Praesent vel blandit ante, nec faucibus
-              libero. Sed ultrices lorem ex, eu facilisis libero convallis ac. Vivamus id dapibus
-              eros. Nullam tempor sem rhoncus porta semper. Proin bibendum vulputate nisl, fringilla
-              interdum elit pulvinar eu. Quisque vitae quam dapibus, vestibulum mauris quis, laoreet
-              massa.
-            </p>
-          </Col>
-        </Row>
-      </Container>
+      <div className="col-sm-8" id="aboutContainer">
+        <div className="card mb-3" style={{ maxWidth: "700px" }}>
+          <div className="row no-gutters">
+            <div className="col-md-12">
+              <div className="card-body">
+                <h1 className="card-title">About Me</h1>
+                <img src="assets/images/IMG_3836.jpeg" className="card-img" id="aboutMeImg"
+                  alt="About Me Image" />
+                <p className="card-text">My background is in operations and project management. For the past
+                    five years
+                    I have worked with both large organizations such as the United States Army as well as
+                    burgeoning
+                    tech start-ups. I am now looking to bring my operational skills to the field of web
+                    development
+                                and help organizations achieve their goals with efficiency and reliability.</p>
+                <br />
+                <p className="card-text"> I have been married to my beautiful wife, Natalie, since May 18th,
+                    2019, and we have a three year old Jack-Russel/Shi-Tsu mix named Scoots. I love doing
+                    all sorts of things, but I especially enjoy spending time with friends and family, being
+                    active, and experimenting in the kitchen. All of that being said I really do find joy in
+                    most things and am always eager to learn something new and try something for the first
+                    time.
+                            </p>
+                <br />
+                <p className="card-text"><strong id="strongLink">Working proficiency in the following
+                                    technologies:</strong>
+                  <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>jQuery</li>
+                    <li>Node.js</li>
+                    <li>MySQL</li>
+                  </ul>
+                </p>
+                <br />
+                <p className="card-text"><strong id="strongLink">Links</strong>
+                  <ul>
+                    <li>
+                      <a href="https://github.com/jcw2865" target="_blank" className="link">GitHub</a>
+                    </li>
+                    <li>
+                      <a href="https://www.linkedin.com/in/jake-walker-22005361/" target="_blank"
+                        className="link">LinkedIn</a>
+                    </li>
+                  </ul>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
