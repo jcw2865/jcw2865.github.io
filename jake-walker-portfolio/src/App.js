5a8437e3bf3971d65fef558096bdfba5c29cf4db
Jake Walker <jcw2865@gmail.com>
2020-02-26 21:35:39 -0600

    HTML routes functional.

--- jake-walker-portfolio/src/App.js
+++ jake-walker-portfolio/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import About from "./pages/About";
 import Wrapper from "./components/Wrapper";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
 
 
 
@@ -13,6 +15,8 @@ function App() {
         <Wrapper>
           <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
         </Wrapper>
       </div>
     </Router>
