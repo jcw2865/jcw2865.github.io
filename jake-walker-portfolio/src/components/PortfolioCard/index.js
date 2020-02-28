89967918269eb7ef958e2228ecdb48c274e36ca8
Jake Walker <jcw2865@gmail.com>
2020-02-27 22:12:39 -0600

    Removed unnecessary line breaks. Changed card height in portfolio.

--- jake-walker-portfolio/src/components/PortfolioCard/index.js
+++ jake-walker-portfolio/src/components/PortfolioCard/index.js
@@ -22,17 +22,14 @@ function PortfolioCard(props) {
                   AS A busy person <br />
                   I WANT to be able to quickly see important information about my day <br />
                   SO THAT I can keep up-to-date with current events and plan my day efficiently.</p>
-                <br />
                 <p className="card-text"> Technologies Used: <br />
                   HTML <br />
                   CSS <br />
                   jQuery <br />
                   JavaScript
                             </p>
-                <br />
                 <a href="https://jcw2865.github.io/Daily-Homepage/" target="_blank" class="card-link">View
                                 Live Site</a>
-                <br />
                 <a href="https://github.com/jcw2865/Daily-Homepage" target="_blank" class="card-link">View
                                 GitHub Repo</a>
               </div>
@@ -47,7 +44,6 @@ function PortfolioCard(props) {
                   AS A a person with interest in politics <br />
                   I WANT to be able to quickly see who my elected officials are <br />
                   SO THAT I can keep up to date with the political climate.</p>
-                <br />
                 <p class="card-text"> Technologies Used: <br />
                   HTML <br />
                   CSS <br />
@@ -56,10 +52,8 @@ function PortfolioCard(props) {
                   Node.js <br />
                   MySQL <br />
                 </p>
-                <br />
                 <a href="https://quiet-meadow-10109.herokuapp.com/" target="_blank" className="card-link">View
                                 Live Site</a>
-                <br />
                 <a href="https://github.com/Phoenix-99hp/Project-2" target="_blank" className="card-link">View
                                 GitHub Repo</a>
               </div>
@@ -74,16 +68,13 @@ function PortfolioCard(props) {
                   AS AN employee with access to sensitive data <br />
                   I WANT to randomly generate a password that meets certain criteria <br />
                   SO THAT I can create a strong password that provides greater security</p>
-                <br />
                 <p className="card-text"> Technologies Used: <br />
                   HTML <br />
                   CSS <br />
                   jQuery <br />
                   JavaScript</p>
-                <br />
                 <a href="https://jcw2865.github.io/Password-Generator/" target="_blank"
                   className="card-link">View Live Site</a>
-                <br />
                 <a href="https://github.com/jcw2865/Password-Generator" target="_blank"
                   class="card-link">View GitHub
                                 Repo</a>
@@ -99,16 +90,13 @@ function PortfolioCard(props) {
                   AS AN employee with a busy schedule <br />
                   I WANT to add important events to a daily planner <br />
                   SO THAT I can manage my time effectively</p>
-                <br />
                 <p className="card-text"> Technologies Used: <br />
                   HTML <br />
                   CSS <br />
                   jQuery <br />
                   JavaScript</p>
-                <br />
                 <a href="https://jcw2865.github.io/Day-Planner/" target="_blank" className="card-link">View Live
                                 Site</a>
-                <br />
                 <a href="https://github.com/jcw2865/Day-Planner" target="_blank" className="card-link">View
                     GitHub
                                 Repo</a>
@@ -125,7 +113,6 @@ function PortfolioCard(props) {
                   I WANT to be able to view and manage the departments, roles, and employees in my company
                                 <br />
                   SO THAT I can organize and plan my business</p>
-                <br />
                 <p className="card-text"> Technologies Used: <br />
                   HTML <br />
                   CSS <br />
@@ -134,7 +121,6 @@ function PortfolioCard(props) {
                   Node.js <br />
                   MySQL <br />
                 </p>
-                <br />
                 <a href="https://github.com/jcw2865/Employee-Tracker" target="_blank" className="card-link">View
                     GitHub
                                 Repo</a>
@@ -151,7 +137,6 @@ function PortfolioCard(props) {
                   I WANT to be able to delete notes I've written before <br />
                   SO THAT I can organize my thoughts and keep track of tasks I need to complete
                             </p>
-                <br />
                 <p className="card-text"> Technologies Used: <br />
                   HTML <br />
                   CSS <br />
@@ -160,10 +145,8 @@ function PortfolioCard(props) {
                   Node.js <br />
                   Express.js <br />
                 </p>
-                <br />
                 <a href="https://shrouded-chamber-01100.herokuapp.com/" target="_blank"
                   className="card-link">View Live Site</a>
-                <br />
                 <a href="https://github.com/jcw2865/Express-Note-Taker" target="_blank"
                   className="card-link">View GitHub
                                 Repo</a>
